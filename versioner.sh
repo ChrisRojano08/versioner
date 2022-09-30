c353db8f7883f8c01020f9864720c4e8010e3e12
ChrisRojano08 <82277319+ChrisRojano08@users.noreply.github.com>
2022-09-30 00:45:59 -0500

    Update versioner.sh

--- versioner.sh
+++ versioner.sh
@@ -3,16 +3,13 @@
 git pull
 git pull origin master
 
-git log --author=autoversioner -n 1
-git log -n 1
-
 version=`git log --author="autoversioner" -n 1`
 echo $version > version.txt
 
-tail -c 6 version.txt
 folderName=`tail -c 6 version.txt`
 mkdir $folderName
 cp -r files/* $folderName/
 
+ls
 rm -r version.txt
-git push origin master
+
