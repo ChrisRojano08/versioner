4fadd3a46874e3506f49798277c71391e5751a17
ChrisRojano08 <crisrojano8@gmail.com>
2022-09-29 22:47:07 -0500

    Making tests v1

---
+++ resources/devops/versioner.js
@@ -0,0 +1,36 @@
+const shell = require('shelljs');
+var regex = new RegExp("^[0-9].[0-9].[0-9]");
+
+let versionAct = validateVer('git log -n 2', regex);
+if(versionAct !== -1){
+    createVer(versionAct);
+}else{
+    versionAct = validateVer('git log -n 3', regex);
+
+    if(versionAct !== -1){
+        createVer(versionAct);
+    }
+}
+
+
+function createVer(version){
+    console.log(version)
+    console.log(shell.exec('ls -l'));
+}
+
+function validateVer(commit, regex){
+    const ultimoCommit = shell.exec(commit);
+    const result = ultimoCommit.indexOf("chore(release)");
+
+    if(result === -1){
+        return '-1';
+    }
+
+    const version = ultimoCommit.substring(result+16, result+21);
+    
+    if(!regex.test(version)){
+        return '-1';
+    }else{
+        return version;
+    }
+}
